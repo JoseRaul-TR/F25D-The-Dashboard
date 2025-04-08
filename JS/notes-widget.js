01ceee6a7d5cdfc9b33c9174be0a978d2b0b7cf3
José_Raúl Tenza_Ramírez <jraultr@gmail.com>
2025-04-08 13:20:30 +0200

    Add notes widget and style it

---
+++ JS/notes-widget.js
@@ -0,0 +1,16 @@
+const noteTextarea = document.getElementById('noteTextarea');
+const dashboardNote = 'dashboardNote';
+
+// Load saved note on page load
+window.onload = function() {
+    const savedNote = localStorage.getItem(dashboardNote);
+    if (savedNote) {
+        noteTextarea.value = savedNote;
+    }
+};
+
+// Save note whenever the content changes
+noteTextarea.addEventListener('input', function() {
+    localStorage.setItem(dashboardNote, this.value);
+});
+
